1e62f935c8b00d5b4476c6c7e8680eb5f590c8d6
JSzyper <jacek.szyper@savangard.com>
2020-06-22 13:55:24 +0200

    zmiany w spring5webapp oraz initial dla petclinic

---
+++ petclinic/src/main/java/pl/phlodx/spring/nauka/petclinic/PetclinicApplication.java
@@ -0,0 +1,13 @@
+package pl.phlodx.spring.nauka.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicApplication.class, args);
+    }
+
+}
