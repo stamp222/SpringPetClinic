4fffc70dbf16f055b9ac206be396b5206f3352d2
JSzyper <jacek.szyper@savangard.com>
2020-07-16 12:39:32 +0200

    add pojos as a model

---
+++ petclinic/src/main/java/pl/phlodx/spring/nauka/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package pl.phlodx.spring.nauka.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ petclinic/src/main/java/pl/phlodx/spring/nauka/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package pl.phlodx.spring.nauka.petclinic.model;
+
+public class Vet extends Person {
+
+}
