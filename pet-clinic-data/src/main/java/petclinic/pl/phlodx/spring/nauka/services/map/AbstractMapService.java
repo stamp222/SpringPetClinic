442a65d92d134cdee3a46b8a5fca31cf6b6ab60b
JSzyper <jacek.szyper@savangard.com>
2020-10-28 14:14:20 +0100

    Update map impl to manage setting of id property closes #23

--- pet-clinic-data/src/main/java/petclinic/pl/phlodx/spring/nauka/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/petclinic/pl/phlodx/spring/nauka/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package petclinic.pl.phlodx.spring.nauka.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import petclinic.pl.phlodx.spring.nauka.model.BaseEntity;
 
-public abstract class AbstractMapService <T, ID> {
-    private Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService <T extends BaseEntity, ID extends Long> {
+    private Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +15,15 @@ public abstract class AbstractMapService <T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        return map.put(id, object);
+    T save(T object) {
+        if(object != null){
+            if (object.getId() == null)
+                object.setId(getNextId());
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
+        return object;
     }
 
     void deleteById(ID id) {
@@ -26,5 +32,15 @@ public abstract class AbstractMapService <T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long id = null;
+        try{
+            id = Collections.max(map.keySet()) + 1;
+        } catch(NoSuchElementException e) {
+            id = 1L;
+        }
+        return id;
     }
 }
--- pet-clinic-web/src/main/java/petclinic/pl/phlodx/spring/nauka/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/petclinic/pl/phlodx/spring/nauka/bootstrap/DataLoader.java
@@ -23,12 +23,10 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Jake");
-        owner1.setId(1L);
         owner1.setLastName("Peralta");
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Emy");
-        owner2.setId(2L);
         owner2.setLastName("Santiago");
 
         ownerService.save(owner1);
@@ -38,12 +36,10 @@ public class DataLoader implements CommandLineRunner {
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Magda");
-        vet1.setId(1L);
         vet1.setLastName("Ptasik");
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jacek");
-        vet2.setId(2L);
         vet2.setLastName("Szyper");
 
         vetService.save(vet1);
