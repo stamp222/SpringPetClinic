1b60e4d0133ebbb70a13333b116f551fef6e07b5
JSzyper <jacek.szyper@savangard.com>
2021-02-12 23:27:32 +0100

    added PetTypeMapService and PetTypeService Closes #29

---
+++ pet-clinic-data/src/main/java/petclinic/pl/phlodx/spring/nauka/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package petclinic.pl.phlodx.spring.nauka.services;
+
+import petclinic.pl.phlodx.spring.nauka.model.PetType;
+
+public interface PetTypeService  extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/petclinic/pl/phlodx/spring/nauka/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package petclinic.pl.phlodx.spring.nauka.services.map;
+
+import org.springframework.stereotype.Service;
+import petclinic.pl.phlodx.spring.nauka.model.PetType;
+import petclinic.pl.phlodx.spring.nauka.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
