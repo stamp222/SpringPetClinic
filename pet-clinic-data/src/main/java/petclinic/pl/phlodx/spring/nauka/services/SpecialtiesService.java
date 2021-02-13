1284aed4a4753a82fed41487c79718920afa82a1
JSzyper <jacek.szyper@savangard.com>
2021-02-13 12:55:02 +0100

    Create Specialty map service impl  Closes #31

---
+++ pet-clinic-data/src/main/java/petclinic/pl/phlodx/spring/nauka/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package petclinic.pl.phlodx.spring.nauka.services;
+
+import petclinic.pl.phlodx.spring.nauka.model.Speciality;
+
+public interface SpecialtiesService extends CrudService <Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/petclinic/pl/phlodx/spring/nauka/services/map/SpecialitiesMapService.java
@@ -0,0 +1,35 @@
+package petclinic.pl.phlodx.spring.nauka.services.map;
+
+import org.springframework.stereotype.Service;
+import petclinic.pl.phlodx.spring.nauka.model.Speciality;
+import petclinic.pl.phlodx.spring.nauka.services.SpecialtiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
